c0e894e6d8790ed808998de7956360fbea76272d
r.tatarinov <r.tatarinov@optimism.ru>
2024-11-09 17:00:57 +0300

    currency_router in interface

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -3,12 +3,14 @@ from fastapi import APIRouter
 from backend.app.routes.card.card import card_router
 from backend.app.routes.cars.cars import car_router
 from backend.app.routes.cbr.cbr import cbr_router
+from backend.app.routes.currency.currency import currency_router
 from backend.app.routes.user.user_router import user_router
 
 router = APIRouter()
 router.include_router(user_router)
 router.include_router(card_router)
 router.include_router(cbr_router)
+router.include_router(currency_router)
 router.include_router(car_router)
 
 
