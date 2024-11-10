40af7588b11f4617611ae26ce5a1b4ec44221185
r.tatarinov <r.tatarinov@optimism.ru>
2024-11-10 13:00:33 +0300

    функционал общих целей

--- backend/app/routes/saving_goals/models.py
+++ backend/app/routes/saving_goals/models.py
@@ -1,7 +1,17 @@
+from fastapi import Query
 from pydantic import BaseModel
 
 
+class GoalParams:
+    def __init__(
+        self,
+        account_id: int = Query(None, description="id накопительного счета")
+    ):
+        self.account_id = account_id
+
+
 class GoalRequest(BaseModel):
+    account_id: int
     description: str
     target_amount: float
     months_remaining: int
--- backend/app/routes/saving_goals/saving_goals.py
+++ backend/app/routes/saving_goals/saving_goals.py
@@ -1,5 +1,6 @@
 from fastapi import Depends, HTTPException, Request, Response, APIRouter
 from fastapi_utils.cbv import cbv
+from sqlalchemy import or_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
@@ -8,7 +9,7 @@ from backend.app.orm_sender.manager_sqlalchemy import ManagerSQLAlchemy
 from backend.app.routes.auth_manager import UserAuthManager
 from backend.app.routes.general_models import GeneralHeadersModel
 from backend.app.routes.main import MainRouterMIXIN
-from backend.app.routes.saving_goals.models import GoalResponse, GoalRequest
+from backend.app.routes.saving_goals.models import *
 from backend.app.routes.saving_goals.response_models import user_goal_responses
 
 goal_router = APIRouter()
@@ -26,23 +27,35 @@ class SavingGoalCBV(UserAuthManager, MainRouterMIXIN, ManagerSQLAlchemy):
         description='Получение информации по поставленной цели',
         tags=goal_tags
     )
-    async def get(self, request: Request, response: Response, headers: GeneralHeadersModel = Depends()):
+    async def get(
+        self,
+        request: Request,
+        response: Response,
+        params: GoalParams = Depends(),
+        headers: GeneralHeadersModel = Depends()
+    ):
         async with AsyncSession(self.engine, autoflush=False, expire_on_commit=False) as session:
             user: User | None = await self.authenticate_user(session, None, None, headers.authorization)
             if not user:
                 return self.make_response_by_error()
 
-            goal_query = await session.execute(select(SavingGoal).filter_by(user_id=user.id))
+            account = await self.get_account(session, user.id, params.account_id)
+            if not account:
+                raise HTTPException(status_code=404, detail="Accumulated account not found")
+
+            goal_query = await session.execute(
+                select(
+                    SavingGoal
+                ).filter_by(
+                    account_id=account.id
+                )
+            )
             goal = goal_query.scalars().first()
             if not goal:
                 raise HTTPException(status_code=404, detail="Saving goal not found")
 
-            return GoalResponse(
-                description=goal.description,
-                target_amount=goal.target_amount,
-                recommended_contribution=goal.recommended_contribution,
-                progress_percentage=goal.progress_percentage
-            )
+            data = self.get_data_by_response_created(goal)
+            return self.get_data(data)
 
     @goal_router.post(
         "/goal",
@@ -56,7 +69,7 @@ class SavingGoalCBV(UserAuthManager, MainRouterMIXIN, ManagerSQLAlchemy):
         self,
         request: Request,
         response: Response,
-        goal_request: GoalRequest,
+        body: GoalRequest,
         headers: GeneralHeadersModel = Depends()
     ):
         async with AsyncSession(self.engine, autoflush=False, expire_on_commit=False) as session:
@@ -64,21 +77,20 @@ class SavingGoalCBV(UserAuthManager, MainRouterMIXIN, ManagerSQLAlchemy):
             if not user:
                 return self.make_response_by_error()
 
-            account_query = await session.execute(select(AccumulatedAccount).filter_by(owner_id=user.id))
-            account = account_query.scalars().first()
+            account = await self.get_account(session, user.id, body.account_id)
             if not account:
                 raise HTTPException(status_code=404, detail="Accumulated account not found")
 
             recommended_contribution = self.calculate_recommended_contribution(
-                goal_request.target_amount, account.amount, goal_request.months_remaining
+                body.target_amount, account.amount, body.months_remaining
             )
-            progress_percentage = self.calculate_progress(account.amount, goal_request.target_amount)
+            progress_percentage = self.calculate_progress(account.amount, body.target_amount)
 
             goal = SavingGoal(
                 user_id=user.id,
                 account_id=account.id,
-                description=goal_request.description,
-                target_amount=goal_request.target_amount,
+                description=body.description,
+                target_amount=body.target_amount,
                 recommended_contribution=recommended_contribution,
                 progress_percentage=progress_percentage
             )
@@ -89,6 +101,22 @@ class SavingGoalCBV(UserAuthManager, MainRouterMIXIN, ManagerSQLAlchemy):
 
             data = self.get_data_by_response_created(goal)
             return self.get_data(data)
+
+    @staticmethod
+    async def get_account(session: AsyncSession, user_id: int, account_id: int) -> AccumulatedAccount | None:
+        account_query = await session.execute(
+            select(
+                AccumulatedAccount
+            ).filter(
+                or_(
+                    AccumulatedAccount.owner_id == user_id,
+                    AccumulatedAccount.invited_users.any(id=user_id)
+                )
+            ).filter_by(
+                id=account_id
+            )
+        )
+        return account_query.scalars().first()
 
     @staticmethod
     def calculate_progress(account_amount: float, target_amount: float) -> float:
